77968ea4ff33c6e04395fe6d65db4b9c9b953786
manojpanta <manojpanta95@gmail.com>
2018-11-26 00:02:19 -0700

    added comments controller

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 describe Article, type: :model do
+  it {should have_many(:comments)}
   it{ should validate_presence_of (:title)}
   it{ should validate_presence_of (:body)}
 end

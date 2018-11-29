c36c82428860b84d09ca2a24946a70489991ddf1
manojpanta <manojpanta95@gmail.com>
2018-11-29 05:04:34 -0700

    removed current admin helper method becausr we dont need to have that there if we not calling that method outside of controllers and done with authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user, :current_admin?
+  helper_method :current_user
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]

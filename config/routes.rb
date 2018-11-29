c7c6eef6d4164f59e8ce3584a1e8534a05ce98e2
manojpanta <manojpanta95@gmail.com>
2018-11-29 04:47:46 -0700

    passing test for admin categories which is only accessibel to admin

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   resources :articles do
     resources :comments
   end
+  namespace :admin do
+    resources :categories, only: [:index]
+  end
   resources :tags
   resources :users, only: [:new, :create, :show]
   get '/login', to: 'sessions#new'
